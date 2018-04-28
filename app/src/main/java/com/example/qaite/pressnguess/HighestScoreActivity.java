485d86ed79236d56ac4929606bc3d2f4d30ff44d
QaiWalker <16014753@myrp.edu.sg>
2018-04-28 21:48:03 +0800

    Initial

---
+++ app/src/main/java/com/example/qaite/pressnguess/HighestScoreActivity.java
@@ -0,0 +1,42 @@
+package com.example.qaite.pressnguess;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class HighestScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highest_score);
+
+        TextView txtScore = findViewById(R.id.textViewCurrentSc);
+        TextView txtHigh = findViewById(R.id.textViewHighestSc);
+
+        Intent intent = getIntent();
+        int score = intent.getIntExtra("score", 0);
+
+        txtScore.setText("Your Score: " + score);
+
+        SharedPreferences mypref = getPreferences(MODE_PRIVATE);
+        int highscore = mypref.getInt("highscore", 0);
+        if(highscore >= score){
+            txtHigh.setText("High Score: " + highscore);
+        } else {
+            txtHigh.setText("New High Score: " + score);
+            SharedPreferences.Editor editor = mypref.edit();
+            editor.putInt("highscore", score);
+            editor.commit();
+        }
+
+
+    }
+    public void onClick (View view){
+        Intent intent = new Intent(HighestScoreActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
