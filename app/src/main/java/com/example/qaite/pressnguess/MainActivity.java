f58109837c58663f2ded62a8637ae91f369397ce
QaiWalker <16014753@myrp.edu.sg>
2018-04-28 17:12:05 +0800

    Initial

--- app/src/main/java/com/example/qaite/pressnguess/MainActivity.java
+++ app/src/main/java/com/example/qaite/pressnguess/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.qaite.pressnguess;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer clarinetSound = MediaPlayer.create(this, R.raw.clarinet);
+
     }
+
+
 }
