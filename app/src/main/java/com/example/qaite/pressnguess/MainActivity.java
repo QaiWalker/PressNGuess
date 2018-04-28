cc7ad54ccc4c63dd91f94e2842cf59cb52d61b1f
QaiWalker <16014753@myrp.edu.sg>
2018-04-28 17:12:43 +0800

    Initial

--- app/src/main/java/com/example/qaite/pressnguess/MainActivity.java
+++ app/src/main/java/com/example/qaite/pressnguess/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MediaPlayer clarinetSound = MediaPlayer.create(this, R.raw.clarinet);
+        final MediaPlayer clarinetSoundMP = MediaPlayer.create(this, R.raw.clarinet);
 
     }
 
